bd2ca9ad659e1addf42befd34d9282fd0198a7e1
Scarlett <lisijia@bu.edu>
2017-03-29 01:44:44 -0400

    code from Cal Lee

--- mapping.R
+++ mapping.R
@@ -1,5 +1,5 @@
 library(ggmap)
-
+  
 load("data.rda")
 get_map(geocode('Back Bay, MA'), zoom=12) -> BOSTON
 ggmap(BOSTON) + geom_point(data, aes(x=long, y=lat), size= 0.1) + scale_size(range=c(0, 0.5))
