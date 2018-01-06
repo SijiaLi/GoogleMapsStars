783396652c8615f2877fc1f4e5fd5cdded5b6c0e
Scarlett <lisijia@bu.edu>
2018-01-06 00:09:08 -0500

    Delete mapping.R

--- mapping.R
+++
@@ -1,6 +0,0 @@
-library(ggmap)
-  
-load("data.rda")
-get_map(geocode('Back Bay, MA'), zoom=12) -> BOSTON
-ggmap(BOSTON) + geom_point(data, aes(x=long, y=lat), size= 0.1) + scale_size(range=c(0, 0.5))
-
